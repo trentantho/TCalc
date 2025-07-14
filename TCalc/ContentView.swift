552ee11376d8ae0c6a3fcaf8b1e233427bc4eabd
Trent Morrison <trent.a.morrison@icloud.com>
2025-07-13 21:55:57 -0400

    Intial Redo; Now with working project builds

---
+++ TCalc/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TCalc
+//
+//  Created by Trent Morrison on 7/13/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TCalc/TCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCalcApp.swift
+//  TCalc
+//
+//  Created by Trent Morrison on 7/13/25.
+//
+
+import SwiftUI
+
+@main
+struct TCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

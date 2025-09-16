bbc316bfef34a5e681a9d4143a3d40dfad59a979
Trent Morrison <trent.a.morrison@icloud.com>
2025-09-15 22:50:21 -0400

    tweaking settings

--- TCalc/Resources/TCalcApp.swift
+++ TCalc/Resources/TCalcApp.swift
@@ -1,17 +1,25 @@
-//
-//  TCalcApp.swift
-//  TCalc
-//
-//  Created by Trent Morrison on 9/15/25.
-//
-
 import SwiftUI
+import SwiftData
 
 @main
 struct TCalcApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
+  var body: some Scene {
+    WindowGroup {
+      TabView {
+        Tab("Calc", systemImage: "command") {
+          CalcView()
+            .frame(minWidth: 300.0, minHeight: 400.0)
         }
+
+        Tab("Settings", systemImage: "gear") {
+          SettingsView()
+            .frame(minWidth: 300.0, minHeight: 400.0)
+        }
+      }
+      .tabViewStyle(.sidebarAdaptable)
+
     }
+    .modelContainer(for: Settings.self)
+
+  }
 }
